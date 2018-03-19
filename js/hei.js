c91fef40c04c4d0dc650c1dec8a441251322191c
guan-cd <renquan520.wo>
2018-03-19 11:10:15 +0800

    hei.js

--- js/hei.js
+++ js/hei.js
@@ -1,9 +1,5 @@
 $(function(){
 
-
-
-
-
     $(window).load(function(){
         var hei = $('.hei').height();
         $('.w-fzlc-con .container').height(hei);
@@ -12,29 +8,21 @@ $(function(){
             $('.w-fzlc-con .container').height(hei1);
         })
 
-
-
         var hei2 = $('.img11').height();
         $('.container-ren').height(hei2);
         console.log(hei2)
 
-
-
-
         /*alert("123")
-        alert(hei2)*/
+         alert(hei2)*/
         $(window).resize(function(){
             hei2 = $('.img11').height();
             $('.container-ren').height(hei2);
         })
+        var boo = $('.iusa img').height();
+        $('.iusa').height(boo)
+        $('.ban-ren').height(boo)
+        
     })
-
-
-
-
-
-
-
     var kuan = $('.kuan').width()
     $('.img12').width(kuan)
 })
